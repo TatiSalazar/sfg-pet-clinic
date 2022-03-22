f412162144ebbf994efaeec7830c6f83e080af09
TatiSalazar <tsalazarp.uq@gmail.com>
2022-03-22 15:32:43 -0500

    inicio proyecto

---
+++ src/test/java/guru/springframework/sfgpetc/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
